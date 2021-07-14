ecc0c95e6cd4befbcf399e3fc610f2bf0f647dfe
asmithasatesh <asmithasatesh@gmail.com>
2021-07-14 14:26:28 +0530

    [Asmitha]Initial commit with Welcome Message

---
+++ Mood Analyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mood_Analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Mood Analyser!");
+        }
+    }
+}
