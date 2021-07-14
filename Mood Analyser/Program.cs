8aa7faa774252528f02bc316dc0f619725499615
asmithasatesh <asmithasatesh@gmail.com>
2021-07-14 15:09:28 +0530

    [Asmitha]Respond to input as Sad or Happy

--- Mood Analyser/Program.cs
+++ Mood Analyser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Mood_Analyser
 {
@@ -6,7 +7,11 @@ namespace Mood_Analyser
     {
         static void Main(string[] args)
         {
+            //Get input statement from user
             Console.WriteLine("Welcome To Mood Analyser!");
+            string[] message = Console.ReadLine().ToLower().Split(" ");
+            MoodAnalyser setmood = new MoodAnalyser(message);
+            Console.WriteLine("Mood is: {0}",setmood.ReturnMessage());
         }
     }
 }
